b41d165edacb8bc469b3c7536fde3a5f07872ee1
quicklybly <quickly.tawer@gmail.com>
2023-11-07 20:33:22 +0300

    add basic mailing

--- src/main/kotlin/com/quicklybly/notificator/alertservice/service/MailAlertingService.kt
+++ src/main/kotlin/com/quicklybly/notificator/alertservice/service/MailAlertingService.kt
@@ -1,11 +1,17 @@
 package com.quicklybly.notificator.alertservice.service
 
 import com.quicklybly.notificator.alertservice.dto.Alert
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
 
 @Service
-class MailAlertingService {
+class MailAlertingService(val javaMailSender: JavaMailSender) {
+
+    @Value("\${spring.mail.username}")
+    private lateinit var mailFrom: String
 
     @KafkaListener(
         topics = ["\${spring.kafka.template.default-topic}"],
@@ -14,6 +20,12 @@ class MailAlertingService {
         containerFactory = "kafkaListenerContainerFactory"
     )
     fun alertsListener(alert: Alert) {
-        println("alert is received: $alert")
+        // todo add recipient validation
+        val mail = SimpleMailMessage().apply {
+            from = mailFrom
+            setTo(alert.recipient)
+            text = alert.message
+        }
+        javaMailSender.send(mail)
     }
 }
